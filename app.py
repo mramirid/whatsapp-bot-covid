62e5ccf7f795258e64aeccdcc93bd7c40cb7df85
Sunu Ilham Pradika <sunudika@gmail.com>
2020-05-16 22:47:59 +0700

    Checkpoint: Read from database

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, request
+from flask import Flask, render_template, request, url_for, redirect
 from twilio.twiml.messaging_response import MessagingResponse
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'whatsapp_bot_covid'
+mysql = MySQL(app)
 
 
 @app.route('/')
 def hello():
-    return 'Hello, World'
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM nasional")
+    data = cur.fetchall()
+    cur.close()
+    return render_template('home.html', datas=data)
 
 
 @app.route('/sms', methods=['POST'])
