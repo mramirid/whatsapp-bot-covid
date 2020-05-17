2f8c90e32bab540720351730291d2e74e6107663
Sunu Ilham Pradika <sunudika@gmail.com>
2020-05-17 20:26:47 +0700

    Add get data: Nasional

--- functions/getter.py
+++ functions/getter.py
@@ -9,19 +9,67 @@ def init_connection(new_mysql):
 
 
 def get_nasional():
+    today = get_today_nasional()
+    yesterday = get_yesterday_nasional()
+
+    # Memgambil index array agar saat pemanggilan variabel mudah, tidak today[0] dst
+    if len(today) > 1:
+        positif = 1
+        sembuh = 2
+        meninggal = 3
+        dalam_perawatan = 4
+        datetime = 6
+    
+        # Penambahan masing2 kasus positif, sembuh & meninggal dari kemarin
+        selisih_positif = today[positif] - yesterday[positif]
+        selisih_sembuh = today[sembuh] - yesterday[sembuh]
+        selisih_meninggal = today[meninggal] - yesterday[meninggal]
+        selisih_perawatan = today[perawatan] - yesterday[perawatan]
+
+        # Selisih total kasus dari kemarin
+        total_yesterday = yesterday[positif] + yesterday[sembuh] + yesterday[meninggal]
+        total_today = today[positif] + today[sembuh] + today[meninggal]
+        selisih_total = total_today - total_yesterday
+
+        message = ''
+
+        if selisih_total > 0:
+            message += 'Statistik kasus di {}\n\n'.format(today[nama_provinsi])  
+            message += '- Positif: {} (+{})\n'.format(today[positif], abs(selisih_positif))
+            message += '- Sembuh: {} (+{})\n'.format(today[sembuh], abs(selisih_sembuh))
+            message += '- Meninggal: {} (+{})\n'.format(today[meninggal], abs(selisih_meninggal))
+            message += '- Dalam perawatan: {} (+{})\n\n'.format(today[perawatan], abs(selisih_perawatan))
+        else:
+            message += 'Statistik kasus di {}\n\n'.format(today[nama_provinsi])  
+            message += '- Positif: {}\n'.format(today[positif])
+            message += '- Sembuh: {}\n'.format(today[sembuh])
+            message += '- Meninggal: {}\n'.format(today[meninggal])
+            message += '- Dalam perawatan: {}\n\n'.format(today[perawatan])
+
+        message += 'Tetap jaga kesehatan dan apabila memungkinkan #DirumahAja\n\n'
+        message += 'Pembaruan terakhir pada {}'.format(today[datetime])
+    else:
+        return False
+    
+    return message
+
+
+
+
+def get_today_nasional():
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM nasional")
+    cur.execute("SELECT * FROM nasional WHERE DATE(created_at) = CURDATE()")
     data = cur.fetchall()
     cur.close()
     return data
 
 
-def get_today_nasional():
-    pass
-
-
 def get_yesterday_nasional():
-    pass
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM nasional WHERE DATE(created_at) = CURDATE()-1")
+    data = cur.fetchall()
+    cur.close()
+    return data
 
 
 ################### End of Nasional ###################
