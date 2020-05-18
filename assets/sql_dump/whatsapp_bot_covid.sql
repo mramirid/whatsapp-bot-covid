62acb6927a4c93109c6f322bba72d6614f61e167
Sunu Ilham Pradika <sunudika@gmail.com>
2020-05-18 15:35:00 +0700

    Update database

--- assets/sql_dump/whatsapp_bot_covid.sql
+++ assets/sql_dump/whatsapp_bot_covid.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `pmhwebid_line_bot_covid`
+-- Database: `whatsapp_bot_covid`
 --
 
 -- --------------------------------------------------------
