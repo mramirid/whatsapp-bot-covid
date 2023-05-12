ec4225dbcab1de1d998812ad01e32f1be9f19849
mramirid <amir.muh.hakim@gmail.com>
2023-05-12 15:26:27 +0700

    chore: remove env vars

--- src/env.schema.ts
+++
@@ -1,7 +0,0 @@
-import Joi, { type InterfaceFrom } from 'types-joi';
-
-export const envSchema = Joi.object({
-  COUNTRY_STATS_URL: Joi.string().required(),
-}).required();
-
-export type Env = InterfaceFrom<typeof envSchema>;
