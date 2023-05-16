8c785d528e7f7358e1c0abd3326ebb8587b9636e
mramirid <amir.muh.hakim@gmail.com>
2023-05-17 05:58:28 +0700

    test: unit test `TwilioResponseInterceptor`

---
+++ src/interceptors/twilio-response.interceptor.spec.ts
@@ -0,0 +1,25 @@
+import type { CallHandler } from '@nestjs/common';
+import { of } from 'rxjs';
+import { twiml } from 'twilio';
+import { TwilioResponseInterceptor } from './twilio-response.interceptor';
+
+describe('TwilioResponseInterceptor', () => {
+  const message = 'Hello World';
+  const messageXML = new twiml.MessagingResponse().message(message).toString();
+
+  const contextMock = {} as any;
+  const callHandlerMock: CallHandler<string> = { handle: () => of(message) };
+  const twilioResponseInterceptor = new TwilioResponseInterceptor();
+
+  it('should convert message to TwiML XML', (done) => {
+    twilioResponseInterceptor
+      .intercept(contextMock, callHandlerMock)
+      .subscribe({
+        next: (value) => {
+          expect(value).toBe(messageXML);
+        },
+        error: done,
+        complete: done,
+      });
+  });
+});
