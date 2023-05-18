984c8646812dbbdc7b96b084129ab980bdbab68a
mramirid <amir.muh.hakim@gmail.com>
2023-05-18 13:44:50 +0700

    test(app): create test suites

--- src/app.service.spec.ts
+++ src/app.service.spec.ts
@@ -2,29 +2,35 @@ import { of } from 'rxjs';
 import { AppService } from './app.service';
 import type { CountryService } from './country/country.service';
 
-describe('AppService - Unit Testing', () => {
-  const getCountryStatsMessageSpy = jest.fn(() => of('COUNTRY_STATS_MESSAGE'));
-  const countryServiceMock: Partial<CountryService> = {
-    getStatsMessage: getCountryStatsMessageSpy,
-  };
-  const appService = new AppService(countryServiceMock as any);
+const getStatsMessageSpy = jest.fn(() => of('COUNTRY_STATS_MESSAGE'));
+const countryServiceMock: Partial<CountryService> = {
+  getStatsMessage: getStatsMessageSpy,
+};
+const appService = new AppService(countryServiceMock as any);
 
+describe('replyHello()', () => {
   it('should reply hello correctly', () => {
     expect(appService.replyHello('Amir')).toMatchSnapshot();
   });
+});
 
+describe('replyHelp()', () => {
   it('should reply help correctly', () => {
     expect(appService.replyHelp()).toMatchSnapshot();
   });
+});
 
+describe('replyUnknown()', () => {
   it('should reply unknown correctly', () => {
     expect(appService.replyUnknown()).toMatchSnapshot();
   });
+});
 
+describe('replyNasional()', () => {
   it('should call CountryService.getStatsMessage() in order to reply nasional', (done) => {
     appService.replyNasional().subscribe({
       next: () => {
-        expect(getCountryStatsMessageSpy).toBeCalled();
+        expect(getStatsMessageSpy).toBeCalled();
       },
       error: done,
       complete: done,
