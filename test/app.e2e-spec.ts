f367873e83d87da2ae82395a2d5e3da6fd5d3a78
mramirid <amir.muh.hakim@gmail.com>
2023-05-20 05:27:17 +0700

    test(app): add end-to-end test for `AppController`

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,84 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { INestApplication } from '@nestjs/common';
-// import * as request from 'supertest';
-// import { AppModule } from './../src/app.module';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import type { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+import { AppController } from '../src/app.controller';
+import { AppService } from '../src/app.service';
+import { dummyCountryStats } from '../src/country/country-stats.dummy';
+import { CountryService } from '../src/country/country.service';
+import { UpstreamAPI } from '../src/upstream-api/upstream-api.abstract';
 
-// describe('AppController (e2e)', () => {
-//   let app: INestApplication;
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
 
-//   beforeEach(async () => {
-//     const moduleFixture: TestingModule = await Test.createTestingModule({
-//       imports: [AppModule],
-//     }).compile();
+  beforeEach(async () => {
+    const cacheManagerMock = {
+      get: async () => dummyCountryStats,
+    };
 
-//     app = moduleFixture.createNestApplication();
-//     await app.init();
-//   });
+    const moduleFixture = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [
+        AppService,
+        CountryService,
+        { provide: CACHE_MANAGER, useValue: cacheManagerMock },
+        { provide: UpstreamAPI, useValue: {} },
+      ],
+    }).compile();
 
-//   it('/ (GET)', () => {
-//     return request(app.getHttpServer())
-//       .get('/')
-//       .expect(200)
-//       .expect('Hello World!');
-//   });
-// });
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('should reply unknown command with the correct WhatsApp message', () => {
+    return request(app.getHttpServer())
+      .post('/')
+      .send({
+        ProfileName: 'Amir',
+        Body: 'Test',
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.text).toMatchSnapshot();
+      });
+  });
+
+  it('should reply /halo command with the correct WhatsApp message', () => {
+    return request(app.getHttpServer())
+      .post('/')
+      .send({
+        ProfileName: 'Amir',
+        Body: '/halo',
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.text).toMatchSnapshot();
+      });
+  });
+
+  it('should reply /help command with the correct WhatsApp message', () => {
+    return request(app.getHttpServer())
+      .post('/')
+      .send({
+        ProfileName: 'Amir',
+        Body: '/help',
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.text).toMatchSnapshot();
+      });
+  });
+
+  it('should reply /nasional command with the correct WhatsApp message', () => {
+    return request(app.getHttpServer())
+      .post('/')
+      .send({
+        ProfileName: 'Amir',
+        Body: '/nasional',
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.text).toMatchSnapshot();
+      });
+  });
+});
